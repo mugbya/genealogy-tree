1dea2cf2b52f1db172d893ade7a72e2b5f97dce2
mugbya <mugbya@gmail.com>
2026-04-28 17:30:59 +0800

    feat:更新
    Signed-off-by: mugbya <mugbya@gmail.com>

--- src-tauri/src/constants.rs
+++ src-tauri/src/constants.rs
@@ -1,5 +1,5 @@
 // 授权服务器地址（代码层面配置）
-pub const LICENSE_SERVER_URL: &str = "http://162.14.99.144:8443";
+pub const LICENSE_SERVER_URL: &str = "http://162.14.99.144";
 
 // 客户端上报地址
-pub const USAGE_REPORT_URL: &str = "http://162.14.99.144:8443/api/license/report";
+pub const USAGE_REPORT_URL: &str = "http://162.14.99.144/api/license/report";
